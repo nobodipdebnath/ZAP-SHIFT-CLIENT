f3280a24756578c365eba63f867806ddd6c1d3bf
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-12-22 20:30:05 +0600

    add success page

--- src/components/About/Success.jsx
+++ src/components/About/Success.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Success = () => {
     return (
-        <div>
-            Success
+        <div className='mt-12 flex flex-col gap-4'>
+            <p className='lg:text-xl text-base text-black-base'>We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
+            </p>
+            <p className='lg:text-xl text-base text-black-base'>We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
+            </p>
+            <p className='lg:text-xl text-base text-black-base'>We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.
+            </p>
         </div>
     );
 };
